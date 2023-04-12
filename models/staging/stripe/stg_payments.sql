6dec8863695a2e6c1ba30da975c73a04f0e4f1f1
Somesh Patel <somesh.patel@lntinfotech.com>
2023-04-12 11:58:31 +0000

    --

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,10 +1,13 @@
 with payments as (
-
     select
+        id as payment_id,
         orderid as order_id,
-        id as customer_id,
-        amount
-        
+        paymentmethod as payment_method,
+        status,
+
+        -- amount is stored in cents, convert it to dollars
+        amount / 100 as amount,
+        created as created_at
     from raw.stripe.payment
 )
 
